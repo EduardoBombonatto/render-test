b6fdb009507d4c8c832c27ffd9a9cd592aec5966
Eduardo Bombonatto <eduardoblorenzetti517@gmail.com>
2024-08-01 15:51:46 -0300

    test

--- models/person.js
+++ models/person.js
@@ -8,10 +8,7 @@ const url = process.env.MONGODB_URI;
 console.log("connecting to", url);
 
 mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(url)
   .then((result) => {
     console.log("connected to MongoDB");
   })
